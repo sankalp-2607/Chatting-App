f380e5e2cf0add6caf249333fdaaa5ae33d82084
sankalp-2607 <ssankalp702@gmail.com>
2025-10-30 15:06:40 +0530

    package upload

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,13 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { google } from "@ai-sdk/google";
+import { convertToModelMessages, streamText } from "ai";
+
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages }: { messages: UIMessage[] } = await req.json();
+  const { messages } = await req.json();
   const result = streamText({
-    model: openai("gpt-5-nano"),
+    model: google("gemini-2.0-flash"),
     messages: convertToModelMessages(messages),
   });
-
   return result.toUIMessageStreamResponse();
 }
